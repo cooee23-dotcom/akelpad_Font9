0e20efc91282e87766977cc8afe865de974b46d1
cooee23-dotcom <cooee23@gmail.com>
2026-09-20 10:57:04 +0100

    Change default preview zoom value from 110 to 100

--- src/akelpad-src/AkelPad.c
+++ src/akelpad-src/AkelPad.c
@@ -508,7 +508,7 @@ POINT ptPreviewScroll;
 DWORD dwPreviewShowDialog=SW_MAXIMIZE;
 int lpZoom[]={25, 50, 75, 100, 150, 200, 300, 400, 500, PREVIEWZOOM_FIT, PREVIEWZOOM_WIDTH};
 int nPreviewZoomMaxIndex=8;
-int nPreviewZoomValue=110;
+int nPreviewZoomValue=100;
 int nPreviewZoomPercent;
 int nPreviewCharHeight;
 int nPreviewAveCharWidth;
